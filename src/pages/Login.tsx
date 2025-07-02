6bec9970dfc7d695f517b63bd00eb6dedfd2e86c
Sebastian Martinez <Luismartinez24210@gmail.com>
2025-07-01 22:34:03 -0500

    Test Deployment Backend

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -21,7 +21,7 @@ const Login: React.FC = () => {
     const loginData = { email, password };
 
     try {
-      const response = await fetch("http://localhost:8080/auth/login", {
+      const response = await fetch(import.meta.env.VITE_USER_API_URL+"/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -39,7 +39,7 @@ const Register: React.FC = () => {
     setError(null);
 
     try {
-      const response = await fetch("http://localhost:8080/auth/register", {
+      const response = await fetch(import.meta.env.VITE_USER_API_URL+"/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
