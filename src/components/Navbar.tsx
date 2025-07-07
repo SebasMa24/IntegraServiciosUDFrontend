78a9dd111887f4ab6386fec2bb0fddd9ac9ec612
Nicolás Sabogal <mew-four@hotmail.com>
2025-07-07 18:18:24 -0500

    refactor: update navigation items in Navbar, change "Reservations" to "Reservas" and comment out "Operation"

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -141,8 +141,8 @@ const Navbar: React.FC = () => {
   const navigationItems = useMemo(() => [
     { to: "/home", icon: "bi-house-door", label: "Home", isPublic: true },
     { to: "/troyadev", icon: "bi-code-slash", label: "Integración", isPublic: true },
-    { to: "/reservations", icon: "bi-calendar-check", label: "Reservations", isPublic: false },
-    { to: "/operation", icon: "bi-gear", label: "Operation", isPublic: false }
+    { to: "/reservations", icon: "bi-calendar-check", label: "Reservas", isPublic: false },
+    // { to: "/operation", icon: "bi-gear", label: "Operation", isPublic: false }
   ], []);
 
   const toggleDropdown = useCallback(() => {
